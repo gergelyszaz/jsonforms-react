23ec12c731d03cf94f587ff9c21213f60158e771
Gergely Száz <gergelyszaz@gmail.com>
2022-10-21 20:54:38 +0000

    Use generated client

--- src/formList/FormList.tsx
+++ src/formList/FormList.tsx
@@ -1,7 +1,35 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
+import {
+  ApiError,
+  FormService,
+  ListItemResponseDTO,
+  OpenAPI,
+} from '../gen/api/client';
 
-export class FormList extends React.Component {
-  render(): React.ReactNode {
-    return <li>Hello</li>;
-  }
+OpenAPI.BASE = 'https://virtserver.swaggerhub.com/gergelyszaz/Forms/1.0.0';
+
+const { listForms } = FormService;
+
+const getForms = async () => {
+  const forms = await listForms();
+  return forms;
+};
+
+export function FormList() {
+  const [items, setItems] = useState<ListItemResponseDTO[]>([]);
+  const [error, setError] = useState<ApiError | null>();
+
+  useEffect(() => {
+    getForms()
+      .then((forms) => setItems(forms.items!))
+      .catch((error) => setError(error));
+  }, []);
+
+  return (
+    <div>
+      {items.map((item: ListItemResponseDTO) => (
+        <li>{item.name}</li>
+      ))}
+    </div>
+  );
 }

0714ee144dcdc9c5516b5dd1c9fd296011c1f13a
Gergely Száz <gergelyszaz@gmail.com>
2022-10-21 00:54:04 +0000

    Extract components to be reused

---
+++ src/dataEditor/DataEditor.tsx
@@ -0,0 +1,36 @@
+import { useState, useMemo } from 'react';
+import { JsonForms } from '@jsonforms/react';
+import schema from './../tmp/schema.json';
+import uischema from './../tmp/uischema.json';
+import {
+  materialCells,
+  materialRenderers,
+} from '@jsonforms/material-renderers';
+
+const initialData = {
+  name: 'Send email to Adrian',
+  description: 'Confirm if you have passed the subject\nHereby ...',
+  done: true,
+  recurrence: 'Daily',
+  rating: 3,
+};
+
+const renderers = [
+  ...materialRenderers,
+  //register custom renderers
+];
+
+export const DataEditor = () => {
+  const [data, setData] = useState<any>(initialData);
+
+  return (
+    <JsonForms
+      schema={schema}
+      uischema={uischema}
+      data={data}
+      renderers={renderers}
+      cells={materialCells}
+      onChange={({ errors, data }) => setData(data)}
+    />
+  );
+};
---
+++ src/navigation/Navigation.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export class Navigation extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <header className='App-header'></header>
+      </div>
+    );
+  }
+}
